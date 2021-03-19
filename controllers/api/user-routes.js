2bb137e08ab6cf6e389912457488629d7fff50f3
fazle <ryan7998@gmail.com>
2021-03-18 23:21:25 -0400

    user api routes created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -43,6 +43,25 @@ router.get('/', (req, res) => {
       });
 });
 
+//Get /api/users/id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    }).then(dbUserData => {
+        if(!dbUserData){
+            res.status(404).json({ message: 'No user found'});
+            return;
+        }
+        res.json(dbUserData);
+    }).catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
 //Post /api/users
 router.post('/', upload.single('profile_img'), (req, res) => {
     User.create({
@@ -56,6 +75,7 @@ router.post('/', upload.single('profile_img'), (req, res) => {
     //     req.session.user_id = dbUserData.id;
     //     req.session.username = dbUserData.username;
     //     req.session.loggedIn = true;
+    // })
         
         res.json(dbUserData);
       })
@@ -64,5 +84,88 @@ router.post('/', upload.single('profile_img'), (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('/login', (req, res) =>{
+    User.findOne({
+        where:{
+            username:req.body.username,
+        }
+    }).then(dbUserData => {
+        if(!dbUserData){
+            res.status(400).json({message: 'No user with that email address!'});
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({message: 'Incorrect password'});
+            return;
+        }
+        // req.session.save(() => {
+        //     req.session.user_id = dbUserData.id;
+        //     req.session.username = dbUserData.username;
+        //     req.session.loggedIn = true;
+        
+            res.json(dbUserData);
+        // });
+    });
+});
+
+// Put /api/users/1
+router.put('/:id', upload.single('profile_img'), (req, res) => {
+    User.update(
+        {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            profile_img: req.file.path
+        },
+        {
+            
+            individualHooks: true,
+            where:{
+                id: req.params.id
+            }
+        }
+    ).then(dbUserData =>{
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Delete /api/users/1
+router.delete('/:id', (req, res) =>{
+    User.destroy({
+        where:{
+            id: req.params.id
+        }
+    }).then(dbUserData =>{
+        if(!dbUserData){
+            res.status(404).json({message:'No user found'});
+            return;
+        }
+        res.json(dbUserData);
+    }).catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Log out:
+router.post('/logout', (req, res) => {
+    // if(req.session.loggedIn){
+    //   req.session.destroy(() => {
+    //     res.status(204).end();
+    //   });
+    // } else {
+    //   res.status(404).end();
+    // }
+})
 
 module.exports = router;
